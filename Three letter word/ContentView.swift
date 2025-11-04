f8c382887937ea4f1d66985cd00f2288bfe4cea6
CCollins1016 <ccollins@bsd220.org>
2025-11-04 10:33:54 -0600

    Initial Commit

---
+++ Three letter word/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Three letter word
+//
+//  Created by Christian Collins on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
